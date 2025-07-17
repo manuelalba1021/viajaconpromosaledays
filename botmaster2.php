c92eb6b332da54dca73347b06b6b983a48d07ee6
manuelalba1021 <manuelalba1021@gmail.com>
2025-07-17 12:39:06 -0500

    Update botmaster2.php

--- botmaster2.php
+++ botmaster2.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
-    'token' => '8011006070:AAH8HM8edmXXufRdFyBX99Wb4_u8ClQsNb4',
-    'chat_id' => '-1002665085901'
+    'token' => '7232965827:AAH28D1_j1lf62ljGSpRHw4O8SCAPJuTXHA',
+    'chat_id' => '-4922343203'
 ];
 header('Content-Type: application/json');
 echo json_encode($config);
